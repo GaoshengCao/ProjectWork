e203d5a2631d9b1faa4811e02154d3ee55bea9d3
CAO GAOSHENG <9697961@itisrossi.edu.it>
2024-12-07 08:02:19 +0100

    Creato Cartella Progetto e aggiunto TODO List

---
+++ CaoGaoshengPrpjectWork/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO
+        //Sistemare Diagramma E/R
+        //Creare WebScraper
+        //Popolare Database
+        //Creare Bot Telegram
+        
+    }
+}
