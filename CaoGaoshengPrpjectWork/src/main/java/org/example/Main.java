6d3d514fbd3fa4d99ac33da20d380ed35c33bc5a
Gaosheng Cao <124038838+GaoshengCao@users.noreply.github.com>
2024-12-14 07:50:50 +0100

    Update TODO list

--- CaoGaoshengPrpjectWork/src/main/java/org/example/Main.java
+++ CaoGaoshengPrpjectWork/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TODO
         //Sistemare Diagramma E/R
+        //Testare un bot generale
         //Creare WebScraper
         //Popolare Database
         //Creare Bot Telegram
