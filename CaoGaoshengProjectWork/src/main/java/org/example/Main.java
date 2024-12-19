3d63b65c2361e1ddf755575ec4e0642b72b23c93
CAO GAOSHENG <9697961@itisrossi.edu.it>
2024-12-19 09:18:53 +0100

    Sistemato Nome Cartella | Aggiornato TODO list | Aggiunto inizio Database

---
+++ CaoGaoshengProjectWork/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        TODO
+        1)Creare Web Scraper
+        2)Utilizzare Web Scraper per controllare dimensione massima
+          delle informazioni da mettere nel database,(Questo per ottimizzarlo)
+        3)Creare Database e controllare Diagramma E/R
+        4)Creare Classe per popolare Database
+        5)Creare Bot Telegram
+        6)Success, Pray that it doesn't break.
+         */
+        
+    }
+}
